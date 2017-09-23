4b7c69386b091d02d4b2977b06dcc6c72dc03a28
gilheec <gilheec@naver.com>
2017-09-23 10:15:38 +0900

    2장 예제 추가

---
+++ node.server.js
@@ -0,0 +1,26 @@
+var http = require('http');
+http.createServer(function(req,res) {
+	res.writeHead(200,{'Content-Type':'text/html'});
+	res.end('<h1>Hello, World</h1>');
+}).listen(52273, function() {
+	console.log('Server running at http://127.0.0.1:52273/');
+});
+
+
+/*
+http.createServer
+(
+	function(req,res) 
+	{
+	    res.writeHead(200,{'Content-Type':'text/html'});
+	    res.end('<h1>Hello, World</h1>');
+    }
+).listen
+(   
+	52273, 
+    function() 
+    {
+	    console.log('Server running at http://127.0.0.1:52273/');
+    }
+);
+*/
