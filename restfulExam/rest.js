568fc78c152513644c72f6ac3fcd14a697a60c70
gilheec <gilheec@naver.com>
2017-09-30 16:10:11 +0900

    안드로이드통신

--- restfulExam/rest.js
+++ restfulExam/rest.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(__dirname+'/public'));
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
