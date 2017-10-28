9ba5d2915e2bc83e787d47d99b50e491caaf6230
gilheec <gilheec@naver.com>
2017-10-28 16:10:59 +0900

    로그인/로그아웃 처리

--- restfulExam/query.sql
+++ restfulExam/query.sql
@@ -9,3 +9,16 @@ password text,
 name text,
 age integer
 );
+
+create table user_login(
+id integer primary key auto_increment,
+user_real_id integer,
+token text,
+created_at DATETIME default current_timestamp
+);
+
+create table user_nologin(
+id integer primary key auto_increment,
+device_token varchar(256) not null UNIQUE,
+created_at DATETIME default current_timestamp
+);
